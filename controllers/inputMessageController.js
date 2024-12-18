8b8f96e431152436a4f3a915b4b4c8416e6ab2ed
Etativel <farhanmaulana4611@gmail.com>
2024-12-18 20:44:13 +0800

    feat: store message on models

--- controllers/inputMessageController.js
+++ controllers/inputMessageController.js
@@ -1,1 +1,27 @@
-module.exports = {};
+const messages = require("../models/message");
+
+module.exports = {
+  getForm: (req, res) => {
+    res.render("form");
+  },
+  getHomepage: (req, res) => {
+    res.render("home.ejs", { title: "Mini Messageboard", messages: messages });
+  },
+  postMessage: (req, res) => {
+    messages.push({
+      text: req.body.messageText,
+      user: req.body.senderName,
+      added: new Date(),
+    });
+
+    res.redirect("/");
+
+    // res.render("backButton", {
+    //   body: {
+    //     name: req.body.senderName,
+    //     message: req.body.messageText,
+    //     date: new Date(),
+    //   },
+    // });
+  },
+};
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,49 +1,16 @@
 const express = require("express");
 const path = require("path");
 const router = express();
-
+const controller = require("../controllers/inputMessageController");
 router.use(express.urlencoded({ extended: true }));
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
-
 // Homepage
-router.get("/", (req, res) => {
-  res.render("home.ejs", { title: "Mini Messageboard", messages: messages });
-});
+router.get("/", controller.getHomepage);
 
 // Get input Form
-router.get("/new", (req, res) => {
-  res.render("form");
-});
+router.get("/new", controller.getForm);
 
 // Post new message
-router.post("/new", (req, res) => {
-  messages.push({
-    text: req.body.messageText,
-    user: req.body.senderName,
-    added: new Date(),
-  });
-
-  res.redirect("/");
-
-  // res.render("backButton", {
-  //   body: {
-  //     name: req.body.senderName,
-  //     message: req.body.messageText,
-  //     date: new Date(),
-  //   },
-  // });
-});
+router.post("/new", controller.postMessage);
 
 module.exports = router;
