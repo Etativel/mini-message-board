29b3e0ed01eca8d19a5bf6d891215e30f6e5366b
Etativel <farhanmaulana4611@gmail.com>
2024-12-24 16:16:29 +0800

    feat: upadate env file

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,9 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
   port: 5432,
 });
